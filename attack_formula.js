79060d59892850a7416dd368facaf1941cf1ebfc
Marc <marc.ribalta@eurecat.org>
2024-11-21 14:59:13 +0100

    maybe?

--- attack_formula.js
+++ attack_formula.js
@@ -201,8 +201,8 @@ class Calculator {
     }
 
     dmg(){
-        let Sdmg = 1 + ((this.attacker.atkHat + this.attacker.atkFairy + this.attacker.atkTitle + this.attacker.atkOil + this.attacker.atkPot + this.attacker.atkSkin 
-            + this.attacker.atkPet + this.attacker.dmgIncreaseTattoo + this.attacker.dmgIncreaseRune + this.attacker.atkCostume + this.attacker.atkFamily) / 100);
+        let Sdmg = (1 + ((this.attacker.atkHat + this.attacker.atkFairy + this.attacker.atkTitle + this.attacker.atkOil + this.attacker.atkPot + this.attacker.atkSkin 
+            + this.attacker.atkPet + this.attacker.dmgIncreaseTattoo + this.attacker.dmgIncreaseRune + this.attacker.atkCostume) / 100))*1.1;
         this.attacker.critDmgTattoo = 1 + (this.attacker.critDmgTattoo / 100);
 
         this.damage.normalDmgMinNormal = (this.damage.normalPdmgMin + this.damage.normalEdmgMin) * Sdmg;
